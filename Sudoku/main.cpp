3345f8671aa9ce099ea593b81d7dac14528b993c
Daniel Leon <DanielLeon@daniel-leons-macbook-pro.local>
2016-10-31 23:30:33 -0400

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Daniel Leon on 2016-10-31.
+//  Copyright © 2016 Daniel Leon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
