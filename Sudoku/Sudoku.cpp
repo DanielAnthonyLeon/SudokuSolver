92369a9c3a0c0d6d4a21d58578ba992f99b0cf08
Daniel Leon <DanielLeon@Daniel-Leons-MacBook-Pro.local>
2016-11-10 16:53:29 -0500

    first commit

---
+++ Sudoku/Sudoku.cpp
@@ -0,0 +1,9 @@
+//
+//  Sudoku.cpp
+//  Sudoku
+//
+//  Created by Daniel Leon on 2016-10-31.
+//  Copyright © 2016 Daniel Leon. All rights reserved.
+//
+
+#include "Sudoku.hpp"
---
+++ Sudoku/Sudoku.hpp
@@ -0,0 +1,14 @@
+//
+//  Sudoku.hpp
+//  Sudoku
+//
+//  Created by Daniel Leon on 2016-10-31.
+//  Copyright © 2016 Daniel Leon. All rights reserved.
+//
+
+#ifndef Sudoku_hpp
+#define Sudoku_hpp
+
+#include <stdio.h>
+
+#endif /* Sudoku_hpp */
